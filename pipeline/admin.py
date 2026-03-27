b62cb6972fd4ac44eeb32895b72e05770b30a73c
Artem Chibisov <artchibisov10@gmail.com>
2026-03-27 15:45:49 +0400

    pipeline migrations

--- pipeline/admin.py
+++ pipeline/admin.py
@@ -1,1 +1,26 @@
 from django.contrib import admin
+
+from .models import CriterionEntry
+
+
+@admin.register(CriterionEntry)
+class CriterionEntryAdmin(admin.ModelAdmin):
+    list_display = (
+        "class_code",
+        "subject_name",
+        "module_number",
+        "teacher_name",
+        "source_sheet_name",
+        "updated_at",
+    )
+    list_filter = ("class_code", "subject_name", "module_number", "teacher_name")
+    search_fields = (
+        "class_code",
+        "subject_name",
+        "teacher_name",
+        "criterion_text",
+        "criterion_text_ai",
+        "source_sheet_name",
+        "source_workbook",
+    )
+    readonly_fields = ("created_at", "updated_at")
--- pipeline/models.py
+++ pipeline/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+
+
+class CriterionEntry(models.Model):
+    class_code = models.CharField(max_length=64)
+    subject_name = models.CharField(max_length=255)
+    teacher_name = models.CharField(max_length=255)
+    module_number = models.PositiveIntegerField()
+    criterion_text = models.TextField()
+    criterion_text_ai = models.TextField(blank=True, default="")
+    source_sheet_name = models.CharField(max_length=255)
+    source_workbook = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["class_code", "subject_name", "module_number", "criterion_text"],
+                name="uq_criterionentry_class_subject_module_criterion",
+            )
+        ]
+        indexes = [
+            models.Index(fields=["class_code", "subject_name"]),
+            models.Index(fields=["module_number"]),
+        ]
+        ordering = ["class_code", "subject_name", "module_number", "criterion_text"]
+
+    def __str__(self) -> str:
+        return f"{self.class_code} / {self.subject_name} / M{self.module_number}"
