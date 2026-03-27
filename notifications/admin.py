c44b05d0249f4b5ba2c4d44bfd7ac6c96821a40e
Artem Chibisov <artchibisov10@gmail.com>
2026-03-27 14:44:05 +0400

    last commit

--- notifications/admin.py
+++ notifications/admin.py
@@ -5,7 +5,7 @@ from .models import TeacherContact
 
 @admin.register(TeacherContact)
 class TeacherContactAdmin(admin.ModelAdmin):
-    list_display = ("name", "chat_id", "is_active", "registration_token")
+    list_display = ("teacher_name", "chat_id", "is_active", "last_seen_at", "registration_token")
     list_filter = ("is_active",)
-    search_fields = ("name", "chat_id", "registration_token")
+    search_fields = ("teacher_name", "chat_id", "registration_token")
     readonly_fields = ("registration_token",)
